13fd0d99cbe495e8a9295d6893a1ae92a2a94ef3
marcos william <downloadmarcos@hotmail.com>
2019-08-06 21:31:14 -0300

    form e list base do categories

---
+++ src/app/pages/categories/categories.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CotegoriesRoutingModule } from './categories-routing.module';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+@NgModule({
+  declarations: [CategoryListComponent, CategoryFormComponent],
+  imports: [
+    CommonModule,
+    CotegoriesRoutingModule
+  ]
+})
+export class CotegoriesModule { }
