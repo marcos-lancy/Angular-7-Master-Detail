ffd12a22e811c6a47a9c157b4153d895dc36336a
marcos william <downloadmarcos@hotmail.com>
2019-08-09 22:19:38 -0300

    Corrigindo ortografia e criando formulário de categoria.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  //quando requisitarem dominio/cateogories [quem responderá é = #CotegoriesModule]
-  {path: 'cateogories', loadChildren: './pages/categories/categories.module#CotegoriesModule'}
+  //quando requisitarem dominio/categories [quem responderá é = #CotegoriesModule]
+  {path: 'categories', loadChildren: './pages/categories/categories.module#CotegoriesModule'}
 ];
 
 @NgModule({
